f6d687b8cf362ee79a4bd5bad9220b3467f4f64d
Abhishek Kumar <abhishek955717@gmail.com>
2025-11-21 15:21:41 +0530

    adding_dialog_box

--- components/Schroll_area.tsx
+++ components/Schroll_area.tsx
@@ -3,18 +3,32 @@ import { ScrollArea } from './ui/scroll-area';
 import { Separator } from './ui/separator';
 import TokenCardReplica from './tokenCard';
 import tokens from "../public/token.json"; 
-// const tags = Array.from({ length: 50 }).map(
-//   (_, i, a) => <TokenCardReplica/>
-// )
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+
 
 const Scroll_Area = () => {
   return (
         <ScrollArea className="h-125 w-full bg-gray-950">
-          <div className="flex flex-col">
-            {tokens.map((item, i) => (
-              <TokenCardReplica data={item} />
-            ))}
-          </div>
+
+          <Dialog>
+            <DialogTrigger>
+              <div className="flex flex-col">
+                {tokens.map((item, i) => (
+                  <TokenCardReplica data={item}/>
+                ))}
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="bg-gray-950 text-white">
+                <DialogTitle className="bg-gray-950 text-white">Coming Soon!</DialogTitle>
+                <DialogDescription className="bg-gray-950 text-white">
+                  This page is underdevelopmet.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          
         </ScrollArea>
 
   );
